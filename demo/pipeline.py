e65f55a5edd276f8df8244ff6fb2edc12494bf93
ethan <ethan.yang@intel.com>
2023-08-24 17:45:12 +0800

    batching the input data

--- demo/pipeline.py
+++ demo/pipeline.py
@@ -37,73 +37,73 @@ if (type(num_cam) == str):
 elif(num_cam > 9):
     print(f'Demo currently supportls 9 streams only')
 else:
-        composite_sinks = {
-            1:"sink_1::alpha=1", 
-            2:"sink_1::xpos=0 sink_2::xpos=640", 
-            3:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360", 
-            4:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360",
-            5:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280",
-            6:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360",
-            7:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720",
-            8:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720 sink_8::xpos=640 sink_8::ypos=720",
-            9:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720 sink_8::xpos=640 sink_8::ypos=720 sink_9::xpos=1280 sink_9::ypos=720"
-            }
-        scales = {
-            1:"video/x-raw,width=640,height=360", 
-            2:"video/x-raw,width=640,height=360", 
-            3:"video/x-raw,width=640,height=360", 
-            4:"video/x-raw,width=640,height=360",
-            5:"video/x-raw,width=640,height=360",
-            6:"video/x-raw,width=640,height=360",
-            7:"video/x-raw,width=640,height=360",
-            8:"video/x-raw,width=640,height=360",
-            9:"video/x-raw,width=640,height=360"
-            }
+    composite_sinks = {
+        1:"sink_1::alpha=1", 
+        2:"sink_1::xpos=0 sink_2::xpos=640", 
+        3:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360", 
+        4:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360",
+        5:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280",
+        6:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360",
+        7:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720",
+        8:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720 sink_8::xpos=640 sink_8::ypos=720",
+        9:"sink_1::xpos=0 sink_2::xpos=640 sink_3::ypos=360 sink_4::xpos=640 sink_4::ypos=360 sink_5::xpos=1280 sink_6::xpos=1280 sink_6::ypos=360 sink_7::ypos=720 sink_8::xpos=640 sink_8::ypos=720 sink_9::xpos=1280 sink_9::ypos=720"
+        }
+    scales = {
+        1:"video/x-raw,width=640,height=360", 
+        2:"video/x-raw,width=640,height=360", 
+        3:"video/x-raw,width=640,height=360", 
+        4:"video/x-raw,width=640,height=360",
+        5:"video/x-raw,width=640,height=360",
+        6:"video/x-raw,width=640,height=360",
+        7:"video/x-raw,width=640,height=360",
+        8:"video/x-raw,width=640,height=360",
+        9:"video/x-raw,width=640,height=360"
+        }
 
-        csink = composite_sinks[num_cam]
-        dsink = ""
+    csink = composite_sinks[num_cam]
+    dsink = ""
 
-        pipeline=""
-        
-        if (display == "yes"):
-            if(target_device=="GPU"):
-                dsink = "videoconvert ! fpsdisplaysink video-sink=xvimagesink sync=false"
-            else:
-                dsink = "videoconvert ! fpsdisplaysink video-sink=xvimagesink sync=false"
+    pipeline=""
+    
+    if (display == "yes"):
+        if(target_device=="GPU"):
+            dsink = "videoconvert ! fpsdisplaysink video-sink=xvimagesink sync=false"
         else:
-            dsink = "gvafpscounter ! fakesink async=false"
+            dsink = "videoconvert ! fpsdisplaysink video-sink=xvimagesink sync=false"
+    else:
+        dsink = "gvafpscounter ! fakesink async=false"
 
-        def input_format(input):
-            input = f'"{input}"'
-            if ("/dev/video" in input):
-                stream = "v4l2src device="+input
-            elif ("://" in input):
-                stream = "urisourcebin buffer-size=4096 uri=" +input
-            else:
-                stream = "filesrc location="+input
-                
-            return stream
+    def input_format(input):
+        input = f'"{input}"'
+        if ("/dev/video" in input):
+            stream = "v4l2src device="+input
+        elif ("://" in input):
+            stream = "urisourcebin buffer-size=4096 uri=" +input
+        else:
+            stream = "filesrc location="+input
+            
+        return stream
 
-        for i in range(num_cam):
-            formatted_input = input_format(list(cams.values())[i])
-            pipeline = pipeline + formatted_input + " ! " + decode_element + " ! gvadetect model="+DL_MODEL+" device=" +target_device+ ""+params+ "" +" ! " + scales[num_cam] + " ! mix.sink_"+str(i+1) + " "
+    for i in range(num_cam):
+        formatted_input = input_format(list(cams.values())[i])
+        pipeline = pipeline + formatted_input + " ! " + decode_element + " ! gvadetect model="+DL_MODEL+" device=" +target_device+ ""+params+ "" +" ! " + scales[num_cam] + " ! mix.sink_"+str(i+1) + " "
 
-        final_pipeline = "compositor name=mix background=1 " + csink + " ! " + dsink + " " + pipeline
-        print(final_pipeline)
+    final_pipeline = "compositor name=mix background=1 " + csink + " ! " + dsink + " " + pipeline
+    print(final_pipeline)
 
-        pipeline = None
-        bus = None
-        message = None
-        
-        Gst.init(None)
-        pipeline = Gst.parse_launch(final_pipeline)
+    pipeline = None
+    bus = None
+    message = None
+    
+    Gst.init(None)
+    pipeline = Gst.parse_launch(final_pipeline)
 
-        # start playing
-        pipeline.set_state(Gst.State.PLAYING)
+    # start playing
+    pipeline.set_state(Gst.State.PLAYING)
 
-        # wait until EOS or error
-        bus = pipeline.get_bus()
+    # wait until EOS or error
+    bus = pipeline.get_bus()
 
-        msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+    msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
 
-        pipeline.set_state(Gst.State.NULL)
+    pipeline.set_state(Gst.State.NULL)
